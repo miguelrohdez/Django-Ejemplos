e6669b56dbea19816c36a47e3ccdd8542a1f3b64
Miguel Alejandro Rojas Hernández <nomeacuerdox4@gmail.com>
2021-06-09 05:53:21 -0500

    PlantillaConContexto

--- Proyecto1/Proyecto1/views.py
+++ Proyecto1/Proyecto1/views.py
@@ -1,5 +1,9 @@
+from typing import ContextManager
 from django.http import HttpResponse
+from django.template import *
 import datetime
+
+from django.template.context import RenderContext
 #Primera vista que devuleve una respuesta
 def HolaMundo(request):
     fecha_act = datetime.datetime.now().year
@@ -30,4 +34,19 @@ def edadFutura(request, anio, edadAct):
             </center>
         </body>
     </html>''' %(anio, edadFutura)
+    return HttpResponse(pagina)
+
+def edadFuturaTemplate(request, anio, edadAct):
+    direfencia = anio - datetime.datetime.now().year
+    edadFutura = edadAct + direfencia
+    #Ruta donde esta la plantilla
+    doc_externo = open("./Templates/PlantillaEdad.html")
+    #Se lee la plantilla
+    plt = Template(doc_externo.read())
+    #Se cierra el documento
+    doc_externo.close()
+    #Se establece el contexto y las variables en caso de tener -> {"clave":valor}
+    contexto = Context({"anioAct":anio,"edadFutura":edadFutura})
+    #Se renderiza la pagina con el contexto
+    pagina = plt.render(contexto)
     return HttpResponse(pagina)
